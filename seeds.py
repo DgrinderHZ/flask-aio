56c08223b44ba07e6e10374bc9eb71fa5a35a557
DgrinderHZ <hassanzekkouri@gmail.com>
2022-07-07 22:54:43 +0100

    fix

--- seeds.py
+++ seeds.py
@@ -4,7 +4,9 @@ from models import BlogPost
 import os
 import re
 
-uri = os.getenv("DATABASE_URL")  # or other relevant config var
+uri = os.getenv("DATABASE_URL")
+print(uri) 
+# or other relevant config var
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 # rest of connection code using the connection string `uri`
