bf9820f9c153edc3c9aac4ed76dede818261abf2
DgrinderHZ <hassanzekkouri@gmail.com>
2022-07-07 14:19:37 +0100

    login required decorator

--- app.py
+++ app.py
@@ -1,13 +1,43 @@
-from flask import Flask, render_template
+
+
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+
+from functools import wraps
+
+from decorators import login_required
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dev'
+
 
 
 @app.route('/')
+@login_required
 def home():
     return render_template('welcome.html')
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials.'
+            return render_template('login.html', error=error)
+        
+        session['logged_in'] = True
+        flash('You were just logged in!')
+        return redirect(url_for('home'))
+    
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    flash('You were just logged out!')
+    return redirect(url_for('home'))
 
 
 
