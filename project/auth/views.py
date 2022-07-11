b236ef073a5f1c49320f13f31eed14c59e007a69
DgrinderHZ <hassanzekkouri@gmail.com>
2022-07-11 18:32:43 +0100

    wtforms

--- project/auth/views.py
+++ project/auth/views.py
@@ -1,6 +1,7 @@
 
 from flask import Blueprint, redirect, render_template, request, session, url_for, flash
 from decorators import login_required
+from .forms import LoginForm
 
 from project.models import User
 
@@ -9,17 +10,19 @@ bp = Blueprint("auth", __name__, template_folder="templates")
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
+    form = LoginForm(request.form)
     if request.method == 'POST':
-        user = User.query.filter_by(name=request.form['username']).first()
-        if user is None or user.password != request.form['password']:
-            error = 'Invalid Credentials.'
-            return render_template('login.html', error=error)
-        
-        session['logged_in'] = True
-        flash('You were just logged in!')
-        return redirect(url_for('home.home'))
-    
-    return render_template('login.html')
+        if form.validate_on_submit():
+            user = User.query.filter_by(name=request.form['username']).first()
+            if user is not None and user.password == request.form['password']:
+                session['logged_in'] = True
+                flash('You were logged in. Go Crazy.')
+                return redirect(url_for('home.home'))
+
+            else:
+                error = 'Invalid username or password.'
+    return render_template('login.html', form=form, error=error)
+
 
 
 @bp.route('/logout')
