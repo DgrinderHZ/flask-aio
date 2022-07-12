ba04441d95db8d03cc3134e03ffbe47fb957e0c7
DgrinderHZ <hassanzekkouri@gmail.com>
2022-07-12 01:40:52 +0100

    keep user

--- drop_db.py
+++ drop_db.py
@@ -7,7 +7,7 @@ db.session.delete(BlogPost.query.get(1))
 db.session.delete(BlogPost.query.get(2))
 db.session.delete(BlogPost.query.get(3))
 db.session.delete(BlogPost.query.get(4))
-db.session.delete(User.query.get(1))
+# db.session.delete(User.query.get(1))
 # commit the changes
 try:
     db.session.commit()
